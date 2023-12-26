9b6d5336138b883c6459adf03ec214ee5bbca63f
kerneltoast <sultan@kerneltoast.com>
2023-12-26 18:27:54 -0500

    eh: Reduce unnecessary wake ups for the compression thread
    Check if the compression thread is actually waiting before attempting to
    wake it up in order to reduce unnecessary wake ups performed for the
    compression thread. Also, move the wake-up outside of the fifo producer
    locked section since it doesn't require the lock.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/soc/google/eh/eh_main.c
+++ drivers/soc/google/eh/eh_main.c
@@ -448,7 +448,10 @@ static void request_to_sw_fifo(struct eh_device *eh_dev,
     list_add_tail(&req->list, &fifo->head);
 	fifo->count++;
 	spin_unlock(&fifo->lock);
-	wake_up(&eh_dev->comp_wq);
+
+	/* spin_unlock() provides a barrier before waitqueue_active() */
+	if (waitqueue_active(&eh_dev->comp_wq))
+		wake_up(&eh_dev->comp_wq);
 }
 
 static int request_to_hw_fifo(struct eh_device *eh_dev, struct page *page,
@@ -474,13 +477,15 @@ static int request_to_hw_fifo(struct eh_device *eh_dev, struct page *page,
 	compl->priv = priv;
 
 	atomic_inc(&eh_dev->nr_request);
-	if (wake_up)
-		wake_up(&eh_dev->comp_wq);
 
 	/* write barrier to force writes to be visible everywhere */
 	wmb();
 	update_fifo_write_index(eh_dev);
 	spin_unlock(&eh_dev->fifo_prod_lock);
+
+	/* spin_unlock() provides a barrier before waitqueue_active() */
+	if (wake_up && waitqueue_active(&eh_dev->comp_wq))
+		wake_up(&eh_dev->comp_wq);
 
 	return 0;
 }
